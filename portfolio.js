66125df2cb66fadb446fc95dd183105123173a0e
Ronak <guliani@pdx.edu>
2023-03-14 19:37:59 -0700

    Write javascript to toggle visibility of navbar

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('nav').style.top = '0';
+  } else {
+    document.querySelector('nav').style.top = '-50px';
+  }
+  prevScrollpos = currentScrollPos;
+};
